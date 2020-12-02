a0e56eb1cd0d4985a4c2133e8d5c29bcfae68113
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2020-12-01 19:40:32 -0500

    slight adjustments

--- day01/day01.py
+++ day01/day01.py
@@ -8,18 +8,18 @@ with open('inputs.txt', 'r') as f:
 expense_report = [float(x) for x in expense_report.split('\n')]
 
 
-def get_result(expense_reports, tally=2020, in_operation=np.sum, out_operation=np.prod):
-    """Given expense_reports: a list of input lists
-    get all permutations of possible combos
+def get_result(expense_report, tally=2020, in_operation=np.sum
+               , out_operation=np.prod, n=2):
+    """Given an expense_report : list
+    get all combinations of expense_report repeated n times
     use in_operation() : must operate on an array
     to find the tally : something that in_operation can yield
     then return the out_operation() on the successful combo
     """
-    possible = itertools.product(*expense_reports)
+    possible = itertools.combinations(expense_report, n)
 
     for group in possible:
         if in_operation(group) == tally:
-            #print(group)
             return int(out_operation(group))
     return None
 
@@ -27,8 +27,7 @@ def get_result(expense_reports, tally=2020, in_operation=np.sum, out_operation=n
 if __name__ == "__main__":
     
     for n in [2, 3]:
-        poss_combos = [expense_report] * n
-        print(get_result(poss_combos))
+        print(get_result(expense_report, n=n))
     
         
     
