1f6657e73d284c40f327457fb17a753ca5ec3d0f
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2020-12-05 11:43:55 -0500

    rename bisect_rows() -> bisect_values()

--- day05/day05.py
+++ day05/day05.py
@@ -1,5 +1,6 @@
 from boarding_passes import all_bps
 
+
 tests = {
     "FBFBBFFRLR": 8*44 + 5,
     "BFFFBBFRRR": 567,
@@ -8,35 +9,35 @@ tests = {
 }
 
 
-def bisect_rows(boarding_pass, lo=0, hi=127, stashed_row=None):
+def bisect_values(boarding_pass, lo=0, hi=127, stashed_row=None):
     """Recursively bisect rows & columns until each is isolated to a single value."""
     # Exit Strategy
     if lo == hi: 
         if not boarding_pass:
             return 8 * stashed_row + lo
         if stashed_row is None:
-            return bisect_rows(boarding_pass, 0, 7, stashed_row=lo)
+            return bisect_values(boarding_pass, 0, 7, stashed_row=lo)
     
     # Recursion Logic
     pointer = boarding_pass[0]
     midpt = (lo + hi) // 2
     
     if pointer in "FL":
-        return bisect_rows(boarding_pass[1:], lo, midpt, stashed_row)
+        return bisect_values(boarding_pass[1:], lo, midpt, stashed_row)
     if pointer in "BR":
-        return bisect_rows(boarding_pass[1:], midpt+1, hi, stashed_row)
+        return bisect_values(boarding_pass[1:], midpt+1, hi, stashed_row)
     raise ValueError("shouldn't ever get here")    
     
 
 for bp, seat_id in tests.items():
-    prediction = bisect_rows(bp)
+    prediction = bisect_values(bp)
     assert prediction == seat_id, f"{bp} failed: {prediction} != {seat_id}"
 print("All tests passed.")
 
 
 if __name__ == "__main__":
     
-    all_seat_ids = set(bisect_rows(bp) for bp in all_bps.split("\n"))
+    all_seat_ids = set(bisect_values(bp) for bp in all_bps.split("\n"))
 
     biggest = max(all_seat_ids)    
     empty_seats = set(range(min(all_seat_ids), biggest)) - all_seat_ids
