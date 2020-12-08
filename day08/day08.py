3771a64142ba44817d9eb7f4a28107a7e4408126
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2020-12-08 14:10:06 -0500

    refactor to remove lambdas from ALL_OPPS and use winning_flag tuple instead of passing info via datatype.

--- day08/day08.py
+++ day08/day08.py
@@ -1,14 +1,6 @@
 from instructions import sample, startup_code
 
 
-ALL_OPPS = {
-    ###           x = (num, pointer)
-    'nop': lambda x: nop(*x)
-    , 'jmp': lambda x: jmp(*x)
-    , 'acc': lambda x: acc(*x)
-}
-
-
 def nop(num, pointer):
     pointer['cur_line'] += 1
     return pointer
@@ -25,6 +17,13 @@ def acc(num, pointer):
     return pointer
 
 
+ALL_OPPS = {
+    'nop': nop
+    , 'jmp': jmp
+    , 'acc': acc
+}
+
+
 def parse_code(text_blob):
     get_code_from_idx = []
     for row in text_blob.split("\n"):
@@ -34,25 +33,31 @@ def parse_code(text_blob):
 
 
 def find_repeated_line(get_code_from_idx):
+    """Iterate over the entire list of codes, returning (accumulator_total, winning_flag)
+    where winning_flag is:
+        False -> if repeats infinitely
+        True  -> if the very last line of code gets executed.
+    """
     seen = set()
     pointer = {'cur_line': 0, 'accumulator': 0}
     
     while True:
         cur_line = pointer['cur_line']
-        # Exit Strategy P2:  transfer "WIN" condition via datatype == string
+        # Exit Strategy P2:  transfer "WIN" condition via winning_flag = True
         if cur_line >= len(get_code_from_idx):
-            return str(pointer['accumulator'])
+            return (pointer['accumulator'], True)
         # Exit Strategy P1: if beginning an infinite loop, exit w/ accumulator total instead
-        if (cur_line in seen):
-            return pointer['accumulator']
+        if cur_line in seen:
+            return (pointer['accumulator'], False)
         seen.add(cur_line)
         process_line(pointer, get_code_from_idx[cur_line])
 
     
 def process_line(pointer, single_code, all_opps=ALL_OPPS):
     """Process a single line of code, return updated 'pointer'"""
-    opp, num = single_code    
-    all_opps[opp]((num, pointer))
+    opp, num = single_code
+    #print(all_opps[opp])
+    all_opps[opp](*(num, pointer))
     return pointer
 
 
@@ -65,23 +70,23 @@ def test_all_jmp_nop(get_code_from_idx):
         bad_cmd, num = get_code_from_idx[idx]
         new_codes = get_code_from_idx.copy()
         new_codes[idx] = replace_cmd[bad_cmd], num
-        accumulated = find_repeated_line(new_codes)
+        accumulated, winning_flag = find_repeated_line(new_codes)
         # Early exit if "WIN" condition found 
-        if isinstance(accumulated, str):
-            return int(accumulated)
+        if winning_flag:
+            return accumulated
         seen.add((idx, accumulated))
     
     return seen
     
 
 get_code_from_idx = parse_code(sample)
-pred = find_repeated_line(get_code_from_idx)
+pred, _ = find_repeated_line(get_code_from_idx)
 assert pred == 5, f"sample failed. {pred} != 5"
 
 
 if __name__ == "__main__":
     get_code_from_idx = parse_code(startup_code)
 
-    print(find_repeated_line(get_code_from_idx))
+    print(find_repeated_line(get_code_from_idx)[0])
     print(test_all_jmp_nop(get_code_from_idx))
     
