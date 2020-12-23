73ff6d01b0da0fbeddd5117b43847f74c310233c
David Kaspar <44875366+UpwardTrajectory@users.noreply.github.com>
2020-12-22 21:24:36 -0500

    finish day 22 part 1, begin work on p2

--- day20/day20.py
+++ day20/day20.py
@@ -1,5 +1,6 @@
 from math import prod
 import re
+import networkx as nx
 from tiles import sample, tiles_and_ids
 
 
@@ -9,6 +10,7 @@ class Tile(dict):
     down: str
     left: str
     right: str
+    img: str
     poss_up: set = set()
     poss_down: set = set()
     poss_left: set = set()
@@ -22,17 +24,23 @@ class Tile(dict):
         down_out = s[-1]
         left_out = ""
         right_out = ""
+        img_out = ""
         
         for row in s[1:]:
             left_out += row[0]
+            img_out += row[1:-1] + "\n"
             right_out += row[-1]
+        
+        img_out = img_out.strip()
+        img_out = img_out[img_out.find("\n") + 1: img_out.rfind("\n")]
             
         final = {
             "ID": id_out,
             "up": up_out,
             "down": down_out,
             "left": left_out,
-            "right": right_out
+            "right": right_out,
+            "img": img_out
         }
         return Tile(final)
     
@@ -74,26 +82,148 @@ class Tile(dict):
             return True
         return False
     
+    def rotate(self):
+        """Rotate 90 degrees anti-clockwise
+        text rotation: https://stackoverflow.com/a/60263655/14083170
+        """
+        up_backup = self['up']
+        self['up'] = self['left']
+        self['left'] = self['down']
+        self['down'] = self['right']
+        self['right'] = up_backup
         
-def solve_puzzle(data):
-    tiles = [Tile.build(s) for s in data.split("\n\n")]
+        poss_up_backup = self['poss_up']
+        self['poss_up'] = self['poss_left']
+        self['poss_left'] = self['poss_down']
+        self['poss_down'] = self['poss_right']
+        self['poss_right'] = poss_up_backup
+        
+        figure = self['img'].split("\n")
+        rotated = list(zip(*reversed(figure)))
+        rotated = "\n".join("".join(c for c in row) for row in rotated)
+        self['img'] = rotated
+        return
     
-    for tile in tiles:
-        other_tiles = tiles.copy()
-        other_tiles.remove(tile)
-        tile['poss_up'] = set([t['ID'] for t in other_tiles if tile['down'] in t.all_possible])
-        tile['poss_down'] = set([t['ID'] for t in other_tiles if tile['up'] in t.all_possible])
-        tile['poss_left'] = set([t['ID'] for t in other_tiles if tile['left'] in t.all_possible])
-        tile['poss_right'] = set([t['ID'] for t in other_tiles if tile['right'] in t.all_possible])
-           
-    return prod([t['ID'] for t in tiles if t.is_corner])
+    def connect(self, other):
+        """Change orientation of <self> until it matches with <other>."""
+        other_search_space = ['poss_up', 'poss_down', 'poss_right', 'poss_left']
+        for way in other_search_space:
+            if self['ID'] in other[way]:
+                target_dir = way[5:]
+                
+        self_to_other = {
+            "up": "down"
+            , "down": "up"
+            , "right": "left"
+            , "left": "right"
+        }
+        
+        other_dir = self_to_other[target_dir]
+        n_times = 0
+        while self[target_dir] != other[other_dir]:
+            self.rotate()
+            
+            n_times +=1
+            if n_times > 12:
+                print("FAILED: Too many rotations.")
+                raise AttributeError("Infinite Loop Avoided.")               
+        return
+    
+    
+class SeaMonsterMap(dict):
+    tiles: list
+    stable: set = set()
+        
+    @staticmethod
+    def build(data):
+        return SeaMonsterMap({'tiles': [Tile.build(s) for s in data.split("\n\n")]})
+    
+    def link_neighbors(self):
+        tiles = self['tiles']
+        
+        for tile in tiles:
+            other_tiles = tiles.copy()
+            other_tiles.remove(tile)
+            tile['poss_up'] = set([t['ID'] for t in other_tiles if tile['down'] in t.all_possible])
+            tile['poss_down'] = set([t['ID'] for t in other_tiles if tile['up'] in t.all_possible])
+            tile['poss_left'] = set([t['ID'] for t in other_tiles if tile['left'] in t.all_possible])
+            tile['poss_right'] = set([t['ID'] for t in other_tiles if tile['right'] in t.all_possible])
+            
+        self['tiles'] = tiles
+        return
+    
+    @property
+    def graph(self):
+        tiles = self['tiles']
+        
+        edges = set()
+        for tile in tiles:
+            neighbors = set()
+            for direction in ['poss_up', 'poss_down', 'poss_left', 'poss_right']:
+                neighbors = neighbors | set([(tile['ID'], other) for other in tile[direction]])
+            edges = edges | neighbors
+        G = nx.Graph()
+        G.add_edges_from(edges)
+        return G    
+    
+    @property
+    def corners(self):
+        """Build a dictionary {"UR": Tile, "UL": Tile, "DR": Tile, "DL": Tile}"""
+        self.link_neighbors()
+        
+        raw_corners = [t for t in self['tiles'] if t.is_corner]
+        
+        corner_goals = {
+            'UR': lambda t: t.up_right
+            , 'UL': lambda t: t.up_left
+            , 'DR': lambda t: t.down_right
+            , 'DL': lambda t: t.down_left
+        }
+        corners = {}
+        
+        for orientation, tile in zip(corner_goals, raw_corners):
+            
+            current_orientation = [lbl for lbl, f in corner_goals.items() if f(tile) == True][0]
+            while current_orientation != orientation:
+                tile.rotate()
+                current_orientation = [lbl for lbl, f in corner_goals.items() if f(tile) == True][0]
+            corners[orientation] = tile
+        return corners                
+    
+    @property
+    def product_of_corners(self):
+        corners = self.corners.values()
+        return prod([t['ID'] for t in corners])
+    
+    def get(self, tile_id):
+        return [t for t in self['tiles'] if t['ID'] == tile_id][0]
+    
+    def fill_one_layer(self):        
+        for stable in self.stable:
+            for neighbor in self.graph.neighbors(stable):
+                if neighbor not in self.stable:
+                    self.get(neighbor).connect(self.get(stable))
+                    self.stable.add(neighbor)
+                    print(f"{neighbor} added. Now at {len(self.stable)}")
+        return
+        
+    def fill_interior(self):
+        """Rotate all tiles until they fit into the puzzle."""   
+        self.stable = self.stable | set([t['ID'] for t in self.corners.values()])
+        
+        while len(self.stable) < len(self['tiles']):
+            self.fill_one_layer()
+        return
+    
+
+m = SeaMonsterMap.build(sample)
+m.fill_interior()
+
+# pred = m.product_of_corners
+# assert pred == 20899048083289
+# print("Test Passed.")
 
 
-
-pred = solve_puzzle(sample)
-assert pred == 20899048083289
-print("Test Passed.")
-
-
-if __name__ == "__main__":
-    print(solve_puzzle(tiles_and_ids))
+# if __name__ == "__main__":
+#     m = SeaMonsterMap.build(tiles_and_ids)
+#     print(m.product_of_corners)
---
+++ day22/day22.py
@@ -0,0 +1,190 @@
+from typing import List
+from dataclasses import dataclass
+from initial_states import sample, starting_decks
+
+
+@dataclass
+class Deck(dict):
+    name: str
+    cards: List[int]
+        
+    @staticmethod
+    def build(data):
+        data = data.split("\n")
+        name_out = data[0].replace(":", "")
+        deck_out = [int(x) for x in data[1:]]
+        return Deck(name_out, deck_out)
+    
+    def replicate_trimmed_self(self):
+        name_out = "_" + self.name
+        deck_out = self.cards[1: self.top+1]
+        return Deck(name_out, deck_out)
+    
+    @property
+    def top(self):
+        return self.cards[0]
+    
+    def starts_recursion(self, other):
+        matched_card_count = self.top <= len(self.cards[1:])
+        other_card_count = other.top <= len(other.cards[1:])
+        other_has_enough = self.top <= len(other.cards)
+        if matched_card_count and other_card_count and other_has_enough:
+            return True
+        return False
+        
+    def win(self, loot):
+        deck = self.cards[1:]
+        self.cards = deck + [self.top, loot]
+        return 
+    
+    def loss(self):
+        self.cards.pop(0)
+        return 
+    
+    @property
+    def score(self):
+        score = [(i+1)*val for i, val in enumerate(self.cards[::-1])]
+        return sum(score)
+            
+    
+def combat(p1, p2):
+    if p1.top > p2.top:
+        winner, loser = p1, p2
+
+    elif p2.top > p1.top:
+        winner, loser = p2, p1
+
+    else:
+        raise NotImplementedError("TIE: not sure what to do. Hopefully we never get here.")
+    winner.win(loser.top)
+    loser.loss()
+    return p1, p2
+    
+    
+@dataclass
+class RecursiveCombat(dict):
+    """Part 2: Remember all previous game-states & recursively play
+    until a winner is found, or the game returns to a previously seen
+    game-state.
+    """
+    p1: Deck
+    p2: Deck
+    deck_histories: set 
+    meta_lvl: str
+        
+    @staticmethod
+    def build(data):
+        myself, friendly_crab = data.split("\n\n")
+    
+        player_1 = Deck.build(myself)
+        player_2 = Deck.build(friendly_crab)
+        return RecursiveCombat(p1=player_1, p2=player_2, deck_histories=set(), meta_lvl="|")
+    
+    def replicate_trimmed_self(self, meta_lvl):
+        player_1 = self.p1.replicate_trimmed_self()
+        player_2 = self.p2.replicate_trimmed_self()
+        meta_out = meta_lvl + "_"
+        return RecursiveCombat(p1=player_1, p2=player_2, deck_histories=set(), meta_lvl=meta_out)
+        
+    def resolve_meta_winner(self, meta_winner, parents):
+        
+        if parents['p1'].name in meta_winner.name:
+            winner, loser = parents['p1'], parents['p2']
+        elif parents['p2'].name in meta_winner.name:
+            loser, winner = parents['p1'], parents['p2']
+        else:
+            print("Parents:", parents['p1'].name, parents['p2'].name)
+            print("Children:", meta_winner.name)
+            raise ValueError(f'MetaWinner: {meta_winner.name} was not cloned from original players {[self.p1.name, self.p2.name]}') 
+        winner.win(loser.top)
+        loser.loss()
+        return parents
+        
+    def play_game(self):
+        
+        p1 = self.p1
+        p2 = self.p2
+        meta_lvl = self.meta_lvl
+        
+        just_exited_recursion = False
+        i = 0
+        while p1.cards and p2.cards:
+#             print(meta_lvl + str(i+1))
+#             print(f"{meta_lvl}  {p1.cards}\n{meta_lvl}  {p2.cards}")
+            game_state = tuple([tuple(p1.cards), tuple(p2.cards)])
+            
+            if (game_state in self.deck_histories) and not just_exited_recursion:
+                print(f"{meta_lvl}Infinite Loop Avoided on round {i} -- Forced Exit with Victory to {self.p1.name}.")
+                if meta_lvl == "|":
+                    print(f"Outermost Game found revisited state on round {i}. {p1.name} wins.")
+                return p1
+            else:
+                self.deck_histories.add(game_state)
+            
+            if p1.starts_recursion(p2):# or p2.starts_recursion(p1):
+                cached_meta_lvl = meta_lvl
+                meta_lvl += "_"             
+                parents = {"p1": p1, "p2": p2}
+                nested_game = self.replicate_trimmed_self(meta_lvl=meta_lvl)
+                meta_winner = nested_game.play_game()
+#                 print(f"Starting recursive game on round {i}")
+#                 print(f"{i}: Parents: {p1.name}, {p2.name}  --  Children:  {meta_p1.name}, {meta_p2.name}")
+#                 meta_winner = self.play_game(p1=meta_p1, p2=meta_p2, meta_lvl=meta_lvl)
+                parents = self.resolve_meta_winner(meta_winner, parents)
+                p1 = parents['p1']
+                p2 = parents['p2']
+                meta_lvl = cached_meta_lvl
+                just_exited_recursion = True
+                
+            else:
+                p1, p2 = combat(p1, p2)
+                just_exited_recursion = False
+            if i > 590 and (meta_lvl == "|"):
+                print(f"{p1.cards}, {p2.cards}")
+            i += 1
+
+        winner = sorted([p1, p2], key=lambda x: len(x.cards))[-1]  
+        if meta_lvl == "|":
+            print(f"{winner.name} WON! Finished after {i} rounds.")
+        return winner
+    
+                           
+def play_game(data):
+    """Part 1: Read raw data, split into two players, and play Combat. 
+    Print the winner & number of rounds it took.
+    Return the winning score.
+    """
+    myself, friendly_crab = data.split("\n\n")
+    
+    player_1 = Deck.build(myself)
+    player_2 = Deck.build(friendly_crab)
+    
+    i = 0
+    while player_1.cards and player_2.cards:
+        combat(player_1, player_2)
+        i += 1
+        if i > 1000:
+            print("Avoiding infinite loop -- Forced exit.")
+            break
+            
+    winner = sorted([player_1, player_2], key=lambda x: len(x.cards))[-1]
+    print(f"{winner.name} WON! Finished after {i} rounds.")
+    return winner.score
+
+
+pred = play_game(sample)
+assert pred == 306
+
+rec_game = RecursiveCombat.build(sample) 
+winner = rec_game.play_game()
+assert winner.score == 291
+
+if __name__ == "__main__":
+    winning_score = play_game(starting_decks)
+    print(winning_score)
+    
+    rec_game = RecursiveCombat.build(starting_decks)
+    winner = rec_game.play_game()
+    print(winner.score)
+
+    # P2 bounds:  (0, 34678)
